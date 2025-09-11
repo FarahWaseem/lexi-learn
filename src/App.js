e2f09e9539ce4ecf8b820afbfed97c1981b2aa56
asma <jadili2482003@gmail.com>
2025-09-11 21:19:33 +0300

    Migrate CRA to vite

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import CombinedLexiLearn from './components/sprints/LexiLearnSprint3Real/LexiLearnSprint3Real';
+import CombinedLexiLearn from './components/CombinedLexiLearn';
+
+
+
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
+      <h1>Hello world</h1>
       <CombinedLexiLearn />
     </div>
   );
